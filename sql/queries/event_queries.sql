1b74455279303b8a89252e3939393ba5fcb082e1
Alexis Zapata Santana <alexis.zapata.santana@gmail.com>
2026-09-04 16:32:35 -0600

    update

--- sql/queries/event_queries.sql
+++ sql/queries/event_queries.sql
@@ -30,22 +30,48 @@ WHERE    status = 'pending'
          OR (status = 'failed'
              AND attempts < max_attempts
              AND next_attempt_at <= Now())
-         OR (locked_until IS NOT NULL
-             AND locked_until <= Now())
-ORDER BY created_at,
-         next_attempt_at LIMIT $1::int
+         OR (
+         status = 'processing'
+         AND attempts < max_attempts
+             AND
+         locked_until IS NOT NULL
+             AND locked_until <= Now()
+            )
+ORDER BY next_attempt_at, created_at
+LIMIT $1::int
 FOR UPDATE SKIP LOCKED;
 
-
--- name: UpdateLockedOutboxEvents :exec
+-- name: ClaimOutboxEvents :exec
 UPDATE outbox_events
 SET locked_by = $1,
 locked_until = $2,
-attempts = $3,
-status = $4,
+attempts = attempts + 1,
+status = 'processing',
 updated_at = NOW()
-FROM unnest($5::UUID[]) AS ids
+FROM unnest($3::UUID[]) AS ids
 WHERE outbox_events.id = ids;
+
+-- name: MarkOutboxEventsAsFailed :exec
+UPDATE outbox_events
+SET status = 'failed',
+last_error = $1,
+next_attempt_at = $2,
+updated_at = NOW(),
+locked_by = NULL,
+locked_until = NULL
+FROM unnest($3::UUID[]) AS ids
+WHERE outbox_events.id = ids;
+
+-- name: MarkOutboxEventsAsPublished :exec
+UPDATE outbox_events
+SET status = 'published',
+published_at = NOW(),
+updated_at = NOW(),
+locked_by = NULL,
+locked_until = NULL
+FROM unnest($1::UUID[]) AS ids
+WHERE outbox_events.id = ids
+AND status = 'processing';
 
 -- name: SetProcessingIncomingEvent :exec
 UPDATE incoming_events
